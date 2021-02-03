8b0e00471bf9c3e4a3e38beded8ac4559d0eb850
Pjery3 <mixxer1265755@gmail.com>
2021-02-03 11:47:35 +0300

    slider

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $('.client__members-wrapper').slick({
+        infinite: false
+    });
+    
+    $('.wrapper .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.wrapper').find('.tab-item').removeClass('active-tab').hide();
+            $('.wrapper .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+});
