366d3834c1e30362dbafcdd4b134cfbf14447115
Pjery3 <mixxer1265755@gmail.com>
2021-02-13 21:19:49 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,34 @@
-$(function(){
+$(function () {
 
     $('.client__members-wrapper').slick({
         infinite: false
     });
-    
-    $('.questions__tabs-wrapper .tab').on('click', function(event) {
+
+    $('.questions__tabs-wrapper .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
-            $('.questions__tabs-wrapper').find('.tab-item').removeClass('active-tab').hide();
-            $('.questions__tabs-wrapper .tabs').find('.tab').removeClass('active');
-            $(this).addClass('active');
-            $('#'+id).addClass('active-tab').fadeIn();
-            return false;
-        });
+        $('.questions__tabs-wrapper').find('.tab-item').removeClass('active-tab').hide();
+        $('.questions__tabs-wrapper .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+    $('.header__burger').on('click', function(){
+        $('.header__nav-menu').slideToggle();
+    })
 });
 
 var acc = document.getElementsByClassName("questions__accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight){
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
     });
-  };
-  
+};
